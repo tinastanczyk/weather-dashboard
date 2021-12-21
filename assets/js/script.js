dab3c9aa3b9daaac3d0411b0725d753ec93ba97b
Tina Stanczyk <cstanczyk123@gmail.com>
2021-12-20 21:32:02 -0500

    Added aesthetic

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ var currentDataEl = document.getElementById("current-data");
 var currentCityEl = document.getElementById("current-city");
 var forecastEl = document.getElementById("forecast-weather");
 var forecastDataEl = document.querySelector("forecast-data");
-
 var dataEl = document.createElement("h4");
 dataEl.textContent = "";
 var tempEl = document.createElement("h5");
@@ -20,7 +19,6 @@ var uvEl = document.createElement("h5");
 uvEl.textContent = "";
 var iconImage = document.createElement("img");
 iconImage.setAttribute("src", "");
-
 var cityHistory = [];
 if (localStorage.getItem("searchHistory")) {
   cityHistory = JSON.parse(localStorage.getItem("searchHistory"));
@@ -38,6 +36,7 @@ function getCityName(event) {
     alert("Please search a city name");
   }
 }
+// This function adds the city searched to an array and saves the array to the local storage
 function addCityToHistory(city) {
   for (i = 0; i < cityHistory.length; i++) {
     if (city.toLowerCase() == cityHistory[i].toLowerCase()) {
@@ -62,13 +61,11 @@ function getCityCoordinates(NameOfCity) {
   fetch(apiURL)
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function (data) {
           // The data received gives the name of the city, latitude and longitude
           var cityName = data[0].name;
           var cityLat = data[0].lat;
           var cityLon = data[0].lon;
-          console.log(data);
           // I pass in these variables to the function that gets the specific coordinate's weather data
           getCityWeather(cityName, cityLat, cityLon);
         });
@@ -93,9 +90,7 @@ function getCityWeather(name, lat, lon) {
   fetch(apiURL)
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
         response.json().then(function (data) {
-          console.log(data.daily);
           displayCurrent(data.daily, name);
           displayFiveDay(data.daily);
         });
@@ -107,7 +102,7 @@ function getCityWeather(name, lat, lon) {
       alert("Unable to connect to Open Weather Map");
     });
 }
-
+// This function changes the background color of the UV Index to indicate whether the rating is mild, moderate or severe
 function indexRating(uvIndex) {
   if (uvIndex >= 0 && uvIndex <= 2) {
     uvEl.style.backgroundColor = "green";
@@ -117,53 +112,43 @@ function indexRating(uvIndex) {
     uvEl.style.backgroundColor = "red";
   }
 }
-
+// This function displays the current weather for the city searched
 function displayCurrent(daily, name) {
-  console.log(name);
   currentCityEl.textContent = name;
   // currentDataEl.classList.add("card");
   // This logs the date of the five-day forecast
-  console.log(daily[0].dt);
   var seconds = daily[0].dt;
   var date = new Date(0);
   date.setUTCSeconds(seconds);
   date.toString("MMM dd, yyyy");
-  console.log(date);
-
   dataEl.textContent = date;
   currentDataEl.appendChild(dataEl);
   // This gets the temperature for the five day forecast
-  console.log(daily[0].temp.day);
   var temp = daily[0].temp.day;
-
   tempEl.textContent = "Temperature: " + temp;
   currentDataEl.appendChild(tempEl);
   // This gets the humidity for the five day forecast
-  console.log(daily[0].humidity);
   var hum = daily[0].humidity;
-
   humEl.textContent = "Humidity: " + hum;
   currentDataEl.appendChild(humEl);
   // This gets the wind speed for the five day forecast
-  console.log(daily[0].wind_speed);
   var windSpeed = daily[0].wind_speed;
-
   windEl.textContent = "Windspeed: " + windSpeed;
   currentDataEl.appendChild(windEl);
   // This gets the UV Index for the five day forecast
-  console.log(daily[0].uvi);
   var uvIndex = daily[0].uvi;
   indexRating(uvIndex);
   uvEl.textContent = "UV Index: " + uvIndex;
+  uvEl.style.padding = "5px";
+  uvEl.style.alignSelf = "center";
   currentDataEl.appendChild(uvEl);
   // This gets the icon from the openweathermap url and appends it as an image to each weather card
-  console.log(daily[0].weather[0].icon);
   var iconCode = daily[0].weather[0].icon;
   var iconSrc = "http://openweathermap.org/img/wn/" + iconCode + ".png";
   iconImage.setAttribute("src", iconSrc);
   iconImage.style.height = "50px";
   iconImage.style.width = "50px";
-  iconImage.style.backgroundColor = "aqua";
+  iconImage.style.backgroundColor = "#0dcaf0";
   currentDataEl.appendChild(iconImage);
 }
 // This function displays the five-day forecast for each city searched
@@ -172,34 +157,28 @@ function displayFiveDay(daily) {
   for (let i = 1; i < 6; i++) {
     document.getElementById("forecast-day-" + i).textContent = "";
     // This logs the date of the five-day forecast
-    console.log(daily[i].dt);
     var seconds = daily[i].dt;
     var date = new Date(0);
     date.setUTCSeconds(seconds);
-    console.log(date);
     var fdataEl = document.createElement("h4");
     fdataEl.textContent = date;
     document.getElementById("forecast-day-" + i).appendChild(fdataEl);
     // This gets the temperature for the five day forecast
-    console.log(daily[i].temp.day);
     var temp = daily[i].temp.day;
     var ftempEl = document.createElement("h5");
     ftempEl.textContent = "Temperature: " + temp;
     document.getElementById("forecast-day-" + i).appendChild(ftempEl);
     // This gets the humidity for the five day forecast
-    console.log(daily[i].humidity);
     var hum = daily[i].humidity;
     var fhumEl = document.createElement("h5");
     fhumEl.textContent = "Humidity: " + hum;
     document.getElementById("forecast-day-" + i).appendChild(fhumEl);
     // This gets the wind speed for the five day forecast
-    console.log(daily[i].wind_speed);
     var windSpeed = daily[i].wind_speed;
     var fwindEl = document.createElement("h5");
     fwindEl.textContent = "Windspeed: " + windSpeed;
     document.getElementById("forecast-day-" + i).appendChild(fwindEl);
     // This gets the UV Index for the five day forecast
-    console.log(daily[i].uvi);
     var fuvIndex = daily[i].uvi;
     var fuvEl = document.createElement("h5");
     fuvEl.textContent = "UV Index: " + fuvIndex;
@@ -218,29 +197,25 @@ function displayFiveDay(daily) {
     ficonImage.setAttribute("src", ficonSrc);
     ficonImage.style.height = "50px";
     ficonImage.style.width = "50px";
-    ficonImage.style.backgroundColor = "aqua";
+    ficonImage.style.backgroundColor = "#0dcaf0";
     document.getElementById("forecast-day-" + i).appendChild(ficonImage);
   }
 }
 // This function prints the city name in the search History and when one of the city's names are clicked, it shows that city's current and forecasted weather
 function printSearchHistory() {
   var searchArray = JSON.parse(localStorage.getItem("searchHistory"));
-  console.log(searchArray);
   searchHistoryEl.textContent = "";
   if (searchArray) {
     for (let i = 0; i < searchArray.length; i++) {
       var historyEl = document.createElement("li");
       historyEl.textContent = searchArray[i];
-      console.log(historyEl);
       historyEl.setAttribute("id", searchArray[i]);
       searchHistoryEl.appendChild(historyEl);
       searchHistoryEl.style.cursor = "pointer";
       historyEl.addEventListener("click", function (event) {
         event.preventDefault();
         var reSearch = event.target;
-        console.log(reSearch);
         var s = reSearch.id;
-        console.log(s);
         getCityCoordinates(s);
       });
     }
@@ -248,8 +223,5 @@ function printSearchHistory() {
     return;
   }
 }
-
 searchFormEl.addEventListener("submit", getCityName);
-
-// getCityCoordinates("Philadelphia");
 printSearchHistory();
